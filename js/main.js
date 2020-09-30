4b282991ece573355729f6f9b53b5afc1fcc9cb3
0msk <snarrpy@mail.ru>
2020-09-30 12:48:23 +0600

    fix hotel and added reviews

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
 	// лооп отвечает за перелистывание слайдов без ограничений
 	loop: true,
 	// сами кнопки перелистывания
 	navigation: {
-		nextEl: '.slider-button__next',
-		prevEl: '.slider-button__prev',
+		nextEl: '.hotel-slider__button__next',
+		prevEl: '.hotel-slider__button__prev',
 	},
 	// переключение слайдов стрелками клавиатуры
 	keyboard: {
 		enabled: true,
 		onlyInViewport: false,
 	},
-})
+});
+var reviewsSlider = new Swiper('.reviews-slider', {
+	loop: true,
+	navigation: {
+		nextEl: '.hotel-slider__button__next',
+		prevEl: '.hotel-slider__button__prev',
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: false,
+	},
+});
