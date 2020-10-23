790ac488a7a71c912f3d9e46e8dab0c3b31d1b14
0msk <snarrpy@mail.ru>
2020-10-23 18:56:39 +0600

    Сверстали мобильное меню.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
+// слайдер hotel
 var hotelSlider = new Swiper('.hotel-slider', {
 	// лооп отвечает за перелистывание слайдов без ограничений
 	loop: true,
-	// сами кнопки перелистывания
+	// кнопки-стрелки перелистывания слайдов
 	navigation: {
 		nextEl: '.hotel-slider__button__next',
 		prevEl: '.hotel-slider__button__prev',
 	},
-	// переключение слайдов стрелками клавиатуры
+	// переключение слайдов с помощью стрелок на клавиатуре
 	keyboard: {
 		enabled: true,
 		onlyInViewport: false,
 	},
 });
+// слайдер reviews
 var reviewsSlider = new Swiper('.reviews-slider', {
 	loop: true,
 	navigation: {
@@ -22,4 +24,14 @@ var reviewsSlider = new Swiper('.reviews-slider', {
 		enabled: true,
 		onlyInViewport: false,
 	},
+});
+// мобильное меню
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+	// выводит в консоль текст при клике по бургеру
+	console.log("Клик по кнопке меню");
+	// при клике по бургеру навешивает модификатор или убирает его
+	document
+		.querySelector(".navbar-bottom")
+		.classList.toggle("navbar-bottom__visible");
 });
