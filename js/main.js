98e7893379a214c9d4b988cb48ef317dffa2e6b7
0msk <snarrpy@mail.ru>
2020-11-18 23:57:16 +0600

    Доделано модальное окно

--- js/main.js
+++ js/main.js
@@ -1,37 +1,58 @@
-// слайдер hotel
-var hotelSlider = new Swiper('.hotel-slider', {
-	// лооп отвечает за перелистывание слайдов без ограничений
-	loop: true,
-	// кнопки-стрелки перелистывания слайдов
-	navigation: {
-		nextEl: '.hotel-slider__button__next',
-		prevEl: '.hotel-slider__button__prev',
-	},
-	// переключение слайдов с помощью стрелок на клавиатуре
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false,
-	},
+$(document).ready(function () {
+
+	// слайдер hotel
+	var hotelSlider = new Swiper('.hotel-slider', {
+		// лооп отвечает за перелистывание слайдов без ограничений
+		loop: true,
+		// кнопки-стрелки перелистывания слайдов
+		navigation: {
+			nextEl: '.hotel-slider__button__next',
+			prevEl: '.hotel-slider__button__prev',
+		},
+		// переключение слайдов с помощью стрелок на клавиатуре
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false,
+		},
+	});
+
+	// слайдер reviews
+	var reviewsSlider = new Swiper('.reviews-slider', {
+		loop: true,
+		navigation: {
+			nextEl: '.reviews-slider__button__next',
+			prevEl: '.reviews-slider__button__prev',
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false,
+		},
+	});
+
+	// мобильное меню
+	var menuButton = $(".menu-button");
+	menuButton.on("click", function () {
+		// при клике по бургеру навешивает модификатор или убирает его
+		$(".navbar-bottom").toggleClass("navbar-bottom__visible");
+	});
+
+	// модальное окно
+	var modalButton = $("[data-toggle=modal]");
+	var closeModalButton = $(".modal__close");
+	modalButton.on("click", openModal);
+	closeModalButton.on("click", closeModal);
+
+	function openModal() {
+		var targetModal = $(this).attr("data-href");
+		$(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+		$(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+	}
+	function closeModal(event) {
+		event.preventDefault();
+		var modalOverlay = $(".modal__overlay");
+		var modalDialog = $(".modal__dialog");
+		modalOverlay.removeClass("modal__overlay--visible");
+		modalDialog.removeClass("modal__dialog--visible");
+	}
 });
-// слайдер reviews
-var reviewsSlider = new Swiper('.reviews-slider', {
-	loop: true,
-	navigation: {
-		nextEl: '.reviews-slider__button__next',
-		prevEl: '.reviews-slider__button__prev',
-	},
-	keyboard: {
-		enabled: true,
-		onlyInViewport: false,
-	},
-});
-// мобильное меню
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-	// выводит в консоль текст при клике по бургеру
-	console.log("Клик по кнопке меню");
-	// при клике по бургеру навешивает модификатор или убирает его
-	document
-		.querySelector(".navbar-bottom")
-		.classList.toggle("navbar-bottom__visible");
-});
+// 23:28. 26 пункт. отмена класса висибл у мадального окна
